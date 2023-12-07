5f05787e575e95988cb824febcffb94b622be290
Nicole <freitasn173@gmail.com>
2023-12-07 11:03:13 -0300

    Arrumando a leitura de entrada caso receba caracteres

--- Menu.c
+++ Menu.c
@@ -2,6 +2,7 @@
 #include "Funcoes.c"
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
     int opcao;
@@ -9,8 +10,8 @@ int main() {
     Lista l;
     inicializarLista(&l);
 
-
     do {
+        printf("\n=============================================\n");
         printf("\nO que deseja fazer?\n");
         printf("1. Inserir nova tarefa\n");
         printf("2. Mostrar tarefas cadastradas\n");
@@ -20,37 +21,52 @@ int main() {
         printf("6. Marcar conclusao de uma tarefa especifica\n");
         printf("7. Salvar lista de tarefas em um arquivo\n");
         printf("8. Sair\n");
+        printf("\n=============================================\n");
 
-        scanf("%d", &opcao);
+        char input[10];
+        int i;
 
-        if (opcao < 1 || opcao > 8) {
-            printf("\nOpcao invalida. Digite uma opcao valida.\n");
-        } else {
-            switch (opcao) {
-                case 1:
-                    inserir_nova_tarefa(&l);
-                    break;
-                case 2:
-                    mostrar_tarefas_cadastradas(&l);
-                    break;
-                case 3:
-                    buscar_por_tarefa(&l);
-                    break;
-                case 4:
-                    editar_informacoes_de_uma_tarefa(&l);
-                    break;
-                case 5:
-                    removerTarefa(&l, id);
-                    break;
-                case 6:
-                    marcar_conclusao_de_uma_tarefa_especifica(&l);
-                    break;
-                case 7:
-                    salvar_lista_de_tarefas_em_um_arquivo(&l);
-                    break;
-                case 8:
-                    encerrar_programa(&l);
-                    break;
+        scanf("%s", input);
+
+        for (i = 0; input[i] != '\0'; i++) {
+            if (!isdigit(input[i])) {
+                printf("Entrada invalida. Digite uma opcao valida.\n");
+                break;
+            }
+        }
+
+        if (i == strlen(input)) {
+            opcao = atoi(input);
+
+            if (opcao < 1 || opcao > 8) {
+                printf("\nOpcao invalida. Digite uma opcao valida.\n");
+            } else {
+                switch (opcao) {
+                    case 1:
+                        inserir_nova_tarefa(&l);
+                        break;
+                    case 2:
+                        mostrar_tarefas_cadastradas(&l);
+                        break;
+                    case 3:
+                        buscar_por_tarefa(&l);
+                        break;
+                    case 4:
+                        editar_informacoes_de_uma_tarefa(&l);
+                        break;
+                    case 5:
+                        removerTarefa(&l, id);
+                        break;
+                    case 6:
+                        marcar_conclusao_de_uma_tarefa_especifica(&l);
+                        break;
+                    case 7:
+                        salvar_lista_de_tarefas_em_um_arquivo(&l);
+                        break;
+                    case 8:
+                        encerrar_programa(&l);
+                        break;
+                }
             }
         }
     } while (opcao != 8);
